da6182de86b36875769d3863a907f723f9ec262e
Christopher A. Mosher <cmosher01@gmail.com>
2019-12-12 23:10:16 -0500

    build docs; add few examples

---
+++ build_docs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+h=docs/examples.html
+echo "<!doctype html><html><body>" >$h
+for a in docs/*.afl ; do
+    ./gradlew run --args="--output=$a.dot $a"
+    dot $a.dot -Tsvg -o $a.svg
+    {
+        echo "<hr><pre><code>"
+        cat $a
+        echo "</code></pre>"
+        echo "<img src=\"../$a.svg\">"
+    }>>$h
+done
+echo "</body></html>" >>$h
